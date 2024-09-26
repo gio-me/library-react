550e74ddfe4e850608f95b28d83c9ae671714d6d
Malik <malik.edwg@gmail.com>
2024-09-26 00:04:28 -0400

    Dynamic routing for books (based on id)

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Footer from "./components/Footer";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Books from "./pages/Books";
+import BookInfo from "./pages/BookInfo";
 import { books } from "./data";
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/books" element={<Books books={books} />} />
+          <Route path="/books/:id" element={<BookInfo books={books} />} />
         </Routes>
         <Footer />
       </div>
---
+++ src/pages/BookInfo.jsx
@@ -0,0 +1,68 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Link } from "react-router-dom";
+import Rating from "../components/ui/Rating.jsx";
+import Price from "../components/ui/Price.jsx";
+
+function BookInfo({ books }) {
+  return (
+    <div id="books__body">
+      <main id="books__main">
+        <div className="books__container">
+          <div className="row">
+            <div className="book__selected--top">
+              <Link to="/books" className="book__link">
+                <FontAwesomeIcon icon="arrow-left" />
+              </Link>
+              <Link to="/books" className="book__link">
+                <h2 className="book__selected--title--top">Books</h2>
+              </Link>
+            </div>
+            <div className="book__selected">
+              <figure className="book__selected--figure">
+                <img
+                  src="https://covers.openlibrary.org/b/id/8091016-L.jpg"
+                  alt=""
+                  className="book__selected--img"
+                />
+              </figure>
+              <div className="book__selected--description">
+                <h2 className="book__selected--title">
+                  Crack the Coding Interview
+                </h2>
+                <Rating rating={4.5} />
+                <div className="book__selected--price">
+                  <Price salePrice={10} originalPrice={20} />
+                </div>
+                <div className="book__summary">
+                  <div className="book__summary--title">Summary</div>
+                  <p className="book__summary__para">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Doloremque aut, hic nobis porro error reiciendis itaque,
+                    sapiente est labore quo magni explicabo libero adipisci
+                    facere, sint asperiores saepe beatae ratione!
+                  </p>
+                  <p className="book__summary__para">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Doloremque aut, hic nobis porro error reiciendis itaque,
+                    sapiente est labore quo magni explicabo libero adipisci
+                    facere, sint asperiores saepe beatae ratione!
+                  </p>
+                </div>
+                <button className="btn">Add to cart</button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="books__container">
+          <div className="row">
+            <div className="book__selected--top">
+              <h2 className="book__selected--title--top">Recommended Books</h2>
+            </div>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+}
+
+export default BookInfo;
