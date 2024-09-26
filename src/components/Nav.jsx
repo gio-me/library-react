fa3e17b5466c9c48300cd2fdb56c54ed8ea1f348
Malik <malik.edwg@gmail.com>
2024-09-25 20:52:33 -0400

    Footer component added & fixed mobile navigation menu

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,6 +2,14 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import LibraryLogo from "../assets/Library.svg";
 
 function Nav() {
+  function openMenu() {
+    document.body.classList += " menu--open";
+  }
+
+  function closeMenu() {
+    document.body.classList.remove("menu--open");
+  }
+
   return (
     <nav>
       <div className="nav__container">
@@ -19,7 +27,7 @@ function Nav() {
               Books
             </a>
           </li>
-          <button className="btn__menu">
+          <button className="btn__menu" onClick={openMenu}>
             <FontAwesomeIcon icon="bars" />
           </button>
           <li className="nav__icon">
@@ -30,7 +38,7 @@ function Nav() {
           </li>
         </ul>
         <div className="menu__backdrop">
-          <button className="btn__menu btn__menu--close">
+          <button className="btn__menu btn__menu--close" onClick={closeMenu}>
             <FontAwesomeIcon icon="times" />
           </button>
           <ul className="menu__links">
